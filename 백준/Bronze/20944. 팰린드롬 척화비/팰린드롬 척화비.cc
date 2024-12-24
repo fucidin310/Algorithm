1e98102533c787a511b02de4441b700389686297
fucidin310 <fucidin310@gmail.com>
2024-12-24 11:03:33 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cout << 'a';
+}
