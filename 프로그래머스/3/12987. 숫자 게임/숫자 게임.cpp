edcabeb288a85dd8c5c8325a482f1d0088ed051b
fucidin310 <fucidin310@gmail.com>
2024-12-31 10:08:11 +0900

    [level 3] Title: 숫자 게임, Time: 12.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    int index = 0;
+    int size = A.size();
+    
+    for(int i = 0; i < size; i++){
+        if(index == size) return answer;
+        
+        while(index < size){
+            if(A[i] < B[index]){
+                index++;
+                answer++;
+                break;
+            }
+            
+            index++;
+        }
+    }
+        
+    return answer;
+}
