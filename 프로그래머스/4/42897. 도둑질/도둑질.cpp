0bbd07198a4d03d18fdca8477caff6e76d373d99
fucidin310 <fucidin310@gmail.com>
2025-03-14 09:39:16 +0900

    [level 4] Title: 도둑질, Time: 6.44 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -13,25 +13,9 @@ int solution(vector<int> money) {
     DP2[1] = money[1];
     
     for(int i = 2; i < money.size(); i++){
-        if(DP1[i - 2] + money[i] > DP1[i - 1]){
-            DP1[i] = DP1[i - 2] + money[i];            
-        }
-        else{
-            DP1[i] = DP1[i - 1];
-        }
-        
-        if(DP2[i - 2] + money[i] > DP2[i - 1]){
-            DP2[i] = DP2[i - 2] + money[i];            
-        }
-        else{
-            DP2[i] = DP2[i - 1];
-        }
+        DP1[i] = max(DP1[i - 2] + money[i], DP1[i - 1]);
+        DP2[i] = max(DP2[i - 2] + money[i], DP2[i - 1]);
     }
     
-    if(DP1[money.size() - 2] > DP2[money.size() - 1]){
-        return DP1[money.size() - 2];
-    }
-    else{
-        return DP2[money.size() - 1];
-    }
+    return max(DP1[money.size() - 2], DP2[money.size() - 1]);
 }
