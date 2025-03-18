d52256a03fac89280f5ef053d8ec5d9e7580cba3
fucidin310 <fucidin310@gmail.com>
2025-03-18 09:16:06 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 25.82 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int answer = 0;
 int N = 0;
 
-void Backtracking(int index, int openNum){
+void DFS(int index, int openNum){
     if(index >= N){
         if(openNum == 0) answer++;
         return;
@@ -14,12 +14,12 @@ void Backtracking(int index, int openNum){
     
     if(N - index < openNum) return;
     
-    Backtracking(index + 1, openNum + 1);
-    if(openNum > 0) Backtracking(index + 1, openNum - 1);
+    DFS(index + 1, openNum + 1);
+    if(openNum > 0) DFS(index + 1, openNum - 1);
 }
 
 int solution(int n) {
     N = n * 2;
-    Backtracking(1, 1);
+    DFS(1, 1);
     return answer;
 }
