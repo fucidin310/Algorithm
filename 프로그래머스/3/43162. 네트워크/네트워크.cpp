f90e8fcb43e8fb2da4d48607b77d6ad03a541b2b
fucidin310 <fucidin310@gmail.com>
2025-11-21 23:26:00 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,38 +1,32 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-int unionArr[200];
-
-int Find(int num){
-    if(unionArr[num] == num) return num;
-    
-    return unionArr[num] = Find(unionArr[num]);
-}
-
-bool Union(int num1, int num2){
-    int unionNum1 = Find(num1);
-    int unionNum2 = Find(num2);
-    
-    if(unionNum1 == unionNum2) return false;
-    else{
-        unionArr[Find(num1)] = unionArr[Find(num2)];
-        return true;
-    }
-}
-
 int solution(int n, vector<vector<int>> computers) {
-    int answer = n;
+    int answer = 0;
+    
+    vector<bool> isVisited;
+    isVisited.resize(n, false);
     
     for(int i = 0; i < n; i++){
-        unionArr[i] = i;
-    }    
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(computers[i][j] == 1){
-                if(Union(i, j)) answer--;
+        if(isVisited[i] == true) continue;
+        
+        stack<int> myStack;
+        myStack.push(i);
+        isVisited[i] = true;
+        answer++;
+        
+        while(!myStack.empty()){
+            int curIndex = myStack.top();
+            myStack.pop();
+            
+            for(int j = 0; j < n; j++){
+                if(isVisited[j] == true || computers[curIndex][j] == 0) continue;
+                
+                myStack.push(j);
+                isVisited[j] = true;
             }
         }
     }
